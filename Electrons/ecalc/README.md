ddc9c62f18d3e77adb0e78d092efa7c2ce46b41a
Danny Hoang <hoangthai0302@gmail.com>
2019-10-20 23:10:49 +0700

    update

---
+++ Electrons/ecalc/README.md
@@ -0,0 +1,55 @@
+<img src="https://raw.githubusercontent.com/elcalc/elcalc/master/logo.png" alt="elcalc logo" align="right" width="200">
+
+# elcalc  [![Build Status](https://travis-ci.org/elcalc/elcalc.svg?branch=master)](https://travis-ci.org/elcalc/elcalc) [![XO code style](https://img.shields.io/badge/code_style-XO-5ed9c7.svg)](https://github.com/xojs/xo) [![Mentioned in Awesome Calculators](https://awesome.re/mentioned-badge.svg)](https://github.com/xxczaki/awesome-calculators)
+
+Elcalc is a cross-platform calculator, built with [Electron](https://electronjs.org/). It is fast, intuitive and can be used as an alternative for the default system calculator. It supports basic arithmetic operations, as well as some scientific functions.
+
+---
+
+## Install
+
+Check out [**the latest release**](https://github.com/elcalc/elcalc/releases/latest).
+
+[![Download elcalc](https://a.fsdn.com/con/app/sf-download-button)](https://sourceforge.net/projects/elcalc-electron/files/latest/download)
+
+---
+
+## Dev
+
+> I recommend using yarn instead of npm.
+
+<img src="https://imgur.com/ORQ3ZoX.png" alt="Screenshot" align="right" width="350"></a>
+
+``` bash
+# Install dependencies
+
+ $ yarn install
+
+# Run the app
+
+ $ yarn start
+ 
+# Run lint & tests
+
+ $ yarn test
+ 
+# Build 
+
+ $ yarn dist
+```
+<a href="https://www.patreon.com/akepinski">
+	<img src="https://c5.patreon.com/external/logo/become_a_patron_button@2x.png" width="160">
+</a>
+
+## Keyboard Support
+
+| Key | Description |
+| ------:| -----------:|
+| <kbd>1</kbd>, <kbd>2</kbd>, <kbd>3</kbd>, <kbd>4</kbd>, <kbd>5</kbd>, <kbd>6</kbd>, <kbd>7</kbd>, <kbd>8</kbd>, <kbd>9</kbd>, <kbd>0</kbd> | Type numbers |
+| <kbd>+</kbd>, <kbd>-</kbd>, <kbd>* or x</kbd>, <kbd>/</kbd>, <kbd>.</kbd>  | Type symbols |
+| <kbd>Enter</kbd> or <kbd>Return</kbd> | Evaluate equation |
+| <kbd>Backspace</kbd> or <kbd>Delete</kbd> | Clear calculator output |
+
+### License
+
+MIT
---
+++ Electrons/ecalc/src/assets/js/app.js
@@ -0,0 +1,184 @@
+const keys = document.querySelectorAll('#calculator span');
+const math = require('mathjs');
+const logger = require('electron-timber');
+
+const operators = ['+', '-', '*', '/'];
+const input = document.querySelector('.screen');
+
+let decimalAdded = true;
+
+/* Buttons */
+
+for (let i = 0; i < keys.length; i++) {
+	keys[i].addEventListener('click', async function (e) {
+		const inputVal = await input.textContent;
+		const btnVal = await this.textContent;
+
+		if (btnVal === 'C') {
+			input.textContent = '';
+			decimalAdded = true;
+			logger.log('Cleared calculator output');
+		} else if (btnVal === '√' && input.textContent === '') {
+			input.textContent = '';
+			decimalAdded = true;
+		} else if (btnVal === 'ʸ√x' && input.textContent === '') {
+			input.textContent = '';
+			decimalAdded = true;
+		} else if (btnVal === '√x' && input.textContent === '') {
+			input.textContent = '';
+			decimalAdded = true;
+		} else if (btnVal === 'xʸ' && input.textContent === '') {
+			input.textContent = '';
+			decimalAdded = true;
+		} else if (btnVal === '%' && input.textContent === '') {
+			input.textContent = '';
+			decimalAdded = true;
+		} else if (btnVal === 'x²' && input.textContent === '') {
+			input.textContent = '';
+			decimalAdded = true;
+		} else if (btnVal === '|x|' && input.textContent === '') {
+			input.textContent = '';
+			decimalAdded = true;
+		} else if (btnVal === 'x!' && input.textContent === '') {
+			input.textContent = '';
+			decimalAdded = true;
+		} else if (btnVal === 'log𝑏(x)' && input.textContent === '') {
+			input.textContent = '';
+			decimalAdded = true;
+		} else if (btnVal !== '' && input.textContent.startsWith('pow(') && !input.textContent.endsWith(')')) {
+			input.textContent += btnVal + ')';
+			decimalAdded = true;
+		} else if (btnVal !== '' && input.textContent.startsWith('nthRoot(') && !input.textContent.endsWith(')')) {
+			input.textContent += btnVal + ')';
+			decimalAdded = true;
+		} else if (btnVal !== '' && input.textContent.startsWith('log(') && !input.textContent.endsWith(')')) {
+			input.textContent += btnVal + ')';
+			decimalAdded = true;
+		} else if (btnVal === '=') {
+			let equation = inputVal;
+			const lastChar = await equation[equation.length - 1];
+
+			if (operators.indexOf(lastChar) > -1 || lastChar === '.') {
+				equation = await equation.replace(/.$/, '');
+			}
+
+			if (equation) {
+				logger.log(`Evaluated equation ${equation}`);
+				input.textContent = await math.eval(equation);
+			}
+
+			decimalAdded = false;
+		} else if (operators.indexOf(btnVal) > -1) {
+			const lastChar = await inputVal[inputVal.length - 1];
+
+			if (inputVal !== '' && operators.indexOf(lastChar) === -1) {
+				input.textContent += btnVal;
+			} else if (inputVal === '' && btnVal === '-') {
+				input.textContent += btnVal;
+			}
+
+			if (operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
+				input.textContent = await inputVal.replace(/.$/, btnVal);
+			}
+
+			decimalAdded = true;
+		} else if (btnVal === '.') {
+			if (decimalAdded == true) {
+				input.textContent += btnVal;
+				decimalAdded = false;
+			}
+		} else if (btnVal === '%') {
+			if (decimalAdded == true) {
+				input.textContent += btnVal;
+				decimalAdded = false;
+			}
+		} else if (btnVal === 'x²') {
+			input.textContent = 'pow(' + input.textContent + ',2)';
+			decimalAdded = false;
+		} else if (btnVal === 'xʸ') {
+			input.textContent = 'pow(' + input.textContent + ',';
+			decimalAdded = false;
+		} else if (btnVal === '√x' && input.textContent !== '') {
+			input.textContent = `sqrt(${input.textContent})`;
+			decimalAdded = false;
+		} else if (btnVal === 'ʸ√x' && input.textContent !== '') {
+			input.textContent = 'nthRoot(' + input.textContent + ',';
+			decimalAdded = false;
+		} else if (btnVal === 'log𝑏(x)' && input.textContent !== '') {
+			input.textContent = 'log(' + input.textContent + ',';
+			decimalAdded = false;
+		} else if (btnVal === '|x|') {
+			input.textContent = 'abs(' + input.textContent + ')';
+			decimalAdded = false;
+		} else if (btnVal === 'x!' && input.textContent !== '') {
+			input.textContent += '!';
+			decimalAdded = false;
+		} else {
+			input.textContent += btnVal;
+		}
+		e.preventDefault();
+	});
+}
+
+/* Keyboard support */
+
+// In this particular case, we need to disable some eslint rules, for keyboard support to work correctly.
+/* eslint eqeqeq:0 */
+/* eslint complexity:0 */
+
+document.addEventListener('keydown', async event => {
+	const {code} = event;
+	const inputVal = await input.textContent;
+	const lastChar = await inputVal[inputVal.length - 1];
+	if (event.shiftKey == false && (code.startsWith('Numpad') || code.startsWith('Digit')) && '0123456789'.includes(code.slice(-1))) {
+		input.textContent += code.slice(-1);
+	}
+
+	if (inputVal != '' && operators.indexOf(lastChar) == -1 && (code == 'Equal' || code == 'NumpadAdd')) {
+		input.textContent += '+';
+		decimalAdded = true;
+	}
+
+	if (operators.indexOf(lastChar) == -1 && (code == 'Minus' || code == 'NumpadSubtract')) {
+		input.textContent += '-';
+		decimalAdded = true;
+	}
+
+	if (inputVal != '' && operators.indexOf(lastChar) == -1 && ((code == 'KeyX' || code == 'NumpadMultiply')) || (code == 'Digit8' && event.shiftKey)) {
+		input.textContent += '*';
+		decimalAdded = true;
+	}
+
+	if (inputVal != '' && operators.indexOf(lastChar) == -1 && (code == 'Slash' || code == 'IntlRo' || code == 'NumpadDivide')) {
+		input.textContent += '/';
+		decimalAdded = true;
+	}
+
+	if ((code == 'Period' || code == 'NumpadDecimal') && decimalAdded == true) {
+		input.textContent += '.';
+		decimalAdded = false;
+	}
+
+	if (inputVal != '' && operators.indexOf(lastChar) == -1 && (code == 'Digit5' && event.shiftKey)) {
+		input.textContent += '%';
+		decimalAdded = true;
+	}
+
+	if (inputVal != '' && operators.indexOf(lastChar) == -1 && (code == 'Digit1' && event.shiftKey)) {
+		input.textContent += '!';
+		decimalAdded = true;
+	}
+
+	if (event.shiftKey == false && (code == 'NumpadEnter' || code == 'Enter')) {
+		const equation = await inputVal.replace(/x/g, '*').replace(/÷/g, '/');
+		input.textContent = await math.eval(equation);
+		decimalAdded = false;
+		logger.log(`Evaluated equation ${equation}`);
+	}
+
+	if (code == 'Backspace' || code == 'Delete') {
+		input.textContent = '';
+		decimalAdded = true;
+		logger.log('Cleared calculator output');
+	}
+});
