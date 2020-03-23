c025d324a98d17ba60eb7c40483a78b35d81c4fa
Danny Hoang <hnthai@humaxdigital.com>
2020-03-23 09:46:49 +0700

    update

--- WebSocket/caro/src/socket.js
+++ WebSocket/caro/src/socket.js
@@ -1,15 +1,40 @@
 import io from 'socket.io-client';
 
-export default function () {
-    const socket = io('http://localhost:5000')
+export const setupWebsocket = ({ host, port }) =>
+    new Promise((resolve) => {
 
-   socket.on('get_list_channels', data => {
-       console.log(data);
-   })
-    socket.on('error', function (err) {
-        console.log('received socket error:')
-        console.log(err)
-    })
+        const socket = io('http://localhost:5000')
+
+        socket.on('connect', () => {
+            socket
+                .emit('authenticate', { token }) //send the jwt
+                .on('authenticated', () => {
+                    //go to home page
+                    resolve(socket);
+                })
+                .on('unauthorized', (msg) => {
+                    console.log(`unauthorized: ${JSON.stringify(msg.data)}`);
+                    throw new Error(msg.data.type);
+                    //hien thi thong bao loi
+                })
+        });
+
+        socket.on('error', function (err) {
+            console.log('received socket error:')
+            console.log(err)
+        })
+    });
+
+
+export default function () {
+    const token = localStorage.getItem('token');
+
+    if(!token) {
+        //redirect to login
+    } else {
+
+        
+    }
 
     return socket;
 
